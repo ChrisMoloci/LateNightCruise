8ddb64954b449e4351557482346f42f9f88f8931
Chris <chrismoloci@gmail.com>
2025-02-25 16:12:04 -0500

    Added some more songs to DB.

--- src/main/java/com/gearshifgroove/late_night_cruise/scenes/Store/Data/DB.java
+++ src/main/java/com/gearshifgroove/late_night_cruise/scenes/Store/Data/DB.java
@@ -9,9 +9,18 @@ public class DB {
         artists = new HashMap<>();
 
         try {
+            artists.put("0000", new Artist("Unknown", "0000"));
+            artists.get("0000").addSong(new Song("0004", "Unknown Genre", "Calm Corporate"));
+
             artists.put("0001", new Artist("Christian Moloci", "0001"));
             artists.get("0001").addSong(new Song("0001", "Lofi", "Intro Song"));
             artists.get("0001").addSong(new Song("0002", "Unknown Genre", "Debug Song"));
+
+            artists.put("0002", new Artist("Aaron Kenny", "0002"));
+            artists.get("0002").addSong(new Song("0003", "Unknown Genre", "Beneath the Moonlight"));
+
+            artists.put("0003", new Artist("Hazy", "0003"));
+            artists.get("0003").addSong(new Song("0005", "Unknown Genre", "Drifting"));
         } catch (Exception e) {
             System.out.println("An error occured while initilizing music database");
             e.printStackTrace();
